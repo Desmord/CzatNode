f49593accd131dcd4b58f481e83d9a3a6bfb7862
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2017-12-11 21:07:25 +0100

    Gotowe zdarzenia uzytkownika bez akutalizacji listy uzytkownikow

--- dev/client/js/czat/displayCzatMethods.js
+++ dev/client/js/czat/displayCzatMethods.js
@@ -1,11 +1,98 @@
-const react = require(`./elementCreator.js`);
 
-let addUserConnectionInfo = ()=>{
-    react.addUserConnectionInfo();
+const getMyTime = () => {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1;
+    let yyyy = today.getFullYear();
+    let hh = today.getHours();
+    let min = today.getMinutes();
+    let sec = today.getSeconds();
+
+    (dd < 10) ? dd = '0' + dd : null;
+    (mm < 10) ? mm = '0' + mm : null;
+    (hh < 10) ? hh = '0' + hh : null;
+    (min < 10) ? min = '0' + min : null;
+    (sec < 10) ? sec = '0' + sec : null;
+
+    today = `${yyyy}-${mm}-${dd}  ${hh}:${min}:${sec}`;
+
+    return today;
+}
+
+let addUserConnectionInfo = (nick) => {
+    let parentElement = document.createElement(`div`);
+    parentElement.classList.add(`chatUserConnectionInfo`);
+    let elementText = document.createTextNode(`${getMyTime()} Użytkownik ${nick} połączony`);
+    parentElement.appendChild(elementText);
+    document.querySelector(`.chat`).appendChild(parentElement);
 }
 
 let addConectionInfo = () => {
-    react.addInfoElement();
+    let parentElement = document.createElement(`div`);
+    parentElement.classList.add(`czatInfo`);
+    let elementText = document.createTextNode(`Połączenie z serwerem nawiązane ${getMyTime()}`);
+    parentElement.appendChild(elementText);
+    document.querySelector(`.chat`).appendChild(parentElement);
+}
+
+let addUserDisconectedInfo = (nick) => {
+    let parentElement = document.createElement(`div`);
+    parentElement.classList.add(`chatUserConnectionInfo`);
+    let elementText = document.createTextNode(`${getMyTime()} Użytkownik ${nick} rozłączony`);
+    parentElement.appendChild(elementText);
+    document.querySelector(`.chat`).appendChild(parentElement);
+}
+
+let addMyMessage = (message, nick) => {
+    let parentElement = document.createElement(`div`);
+    parentElement.classList.add(`myMessage`);
+
+    let messageTitle =document.createElement(`div`);
+    messageTitle.classList.add(`messageTitle`);
+
+    let titleText = document.createTextNode(`${getMyTime()} -  `);
+    let nickElement =document.createElement(`div`);
+    let nickElementText = document.createTextNode(`${nick}`);
+
+    messageTitle.appendChild(titleText);
+    nickElement.appendChild(nickElementText);
+    messageTitle.appendChild(nickElement);
+    parentElement.appendChild(messageTitle);
+
+    let messageTextElement = document.createElement(`div`);
+    messageTextElement.classList.add(`messageText`);
+    let messageText = document.createTextNode(`${message}`);
+    messageTextElement.appendChild(messageText);
+
+    parentElement.appendChild(messageTextElement);
+    
+    document.querySelector(`.chat`).appendChild(parentElement);
+}
+
+let addOtherMessage = (messageObject) => {
+    let parentElement = document.createElement(`div`);
+    parentElement.classList.add(`othersMessage`);
+
+    let messageTitle =document.createElement(`div`);
+    messageTitle.classList.add(`messageTitle`);
+
+    let titleText = document.createTextNode(`${getMyTime()} -  `);
+    let nickElement =document.createElement(`div`);
+    let nickElementText = document.createTextNode(`${messageObject.nick}`);
+
+    messageTitle.appendChild(titleText);
+    nickElement.appendChild(nickElementText);
+    messageTitle.appendChild(nickElement);
+    parentElement.appendChild(messageTitle);
+
+    let messageTextElement = document.createElement(`div`);
+    messageTextElement.classList.add(`messageText`);
+    let messageText = document.createTextNode(`${messageObject.message}`);
+    messageTextElement.appendChild(messageText);
+
+    parentElement.appendChild(messageTextElement);
+    
+    document.querySelector(`.chat`).appendChild(parentElement);
 }
 
 let hideLoginContainer = () => {
@@ -33,5 +120,8 @@ let changeContainer = () => {
 module.exports = {
     changeContainer: changeContainer,
     addConectionInfo: addConectionInfo,
-    addUserConnectionInfo: addUserConnectionInfo
+    addUserConnectionInfo: addUserConnectionInfo,
+    addUserDisconectedInfo: addUserDisconectedInfo,
+    addMyMessage: addMyMessage,
+    addOtherMessage: addOtherMessage
 }
--- dev/client/js/czat/userEvents.js
+++ dev/client/js/czat/userEvents.js
@@ -2,10 +2,45 @@ const displayHideMethods = require(`./displayCzatMethods.js`);
 
 /** Seting user czat event and displeing czat container */
 class UserEvents {
-    constructor() { }
+    constructor() { 
+        this.userArray = [];
+    }
+
+    addUser(){
+
+    }
+
+    removeUser(){
+        
+    }
 
     getLoginValue(){
         return document.querySelector(`#login`).value;
+    }
+
+    setEnterEvent(socket){
+        document.addEventListener('keypress', (e) => {
+            let keyname = e.key;
+            if (keyname === 'Enter') {
+                e.preventDefault();
+                socket.emit(`message`,document.querySelector(`#czatTextArea`).value);
+                displayHideMethods.addMyMessage(document.querySelector(`#czatTextArea`).value,this.getLoginValue());
+                document.querySelector(`.chat`).scrollTop = document.querySelector(`.chat`).scrollHeight;
+            }
+        });
+    }
+
+    setClickEvent(socket){
+        document.querySelector(`.chatSendButton`).addEventListener(`click`,()=>{
+            socket.emit(`message`,document.querySelector(`#czatTextArea`).value);
+            displayHideMethods.addMyMessage(document.querySelector(`#czatTextArea`).value,this.getLoginValue());
+            document.querySelector(`.chat`).scrollTop = document.querySelector(`.chat`).scrollHeight;
+        });
+    }
+
+    setButtonEvents(socket){
+        this.setClickEvent(socket);
+        this.setEnterEvent(socket);
     }
 
     logIn(socket) {
@@ -13,27 +48,24 @@ class UserEvents {
             displayHideMethods.changeContainer();
             displayHideMethods.addConectionInfo();
 
+            this.setButtonEvents(socket);
+
             socket.emit(`userConnect`,this.getLoginValue());
 
             socket.on(`userConnection`, (nick) => {
                 console.log(`nowy user ${nick}`);
-               displayHideMethods.addUserConnectionInfo();
+               displayHideMethods.addUserConnectionInfo(nick);
             });
 
-            // socket.on(`userDisconected`, (nick) => {
-            //     console.log(`Uzytkownik ${nick} rozłaczony`);
-            // });
+            socket.on(`userDisconected`, (nick) => {
+                console.log(`Uzytkownik ${nick} rozłaczony`);
+                displayHideMethods.addUserDisconectedInfo(nick);
+            });
 
-            // socket.on('message', (messageObject) => {
-            //     console.log(messageObject);
-            // });
-
-
-            // socket.emit(`userConnect`,`MikołajNick`);
-
-            // 6
-// //wysywalnie wiadomosci
-// socket.emit(`message`,`Wiadomosc`);
+            socket.on('message', (messageObject) => {
+                displayHideMethods.addOtherMessage(messageObject);
+                document.querySelector(`.chat`).scrollTop = document.querySelector(`.chat`).scrollHeight;
+            });
 
         });
     }
